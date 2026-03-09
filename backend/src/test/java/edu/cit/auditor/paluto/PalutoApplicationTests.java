1a5c3f592e6d2b317f8a9828715ee86a5b6ad207
LAB24_USER ON WS44 <L24X07W44@LabHCI.net>
2026-03-09 08:17:55 +0800

    setup(backend): setup

---
+++ backend/src/test/java/edu/cit/auditor/paluto/PalutoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.auditor.paluto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
