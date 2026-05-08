c2fe377aa486575dc0f0e4ed1ac90a34c2de63e4
Mechole17 <232763660+Mechole17@users.noreply.github.com>
2026-05-09 01:28:37 +0800

    feature(supabase): integrated supabase as storage

--- backend/src/main/java/edu/cit/auditor/paluto/entity/User.java
+++ backend/src/main/java/edu/cit/auditor/paluto/entity/User.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
