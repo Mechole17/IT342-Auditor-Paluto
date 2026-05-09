8c94b6f0678870dcbaf29473d2da51fe99f0e07e
Mechole17 <232763660+Mechole17@users.noreply.github.com>
2026-05-09 17:29:38 +0800

    refactor: add argsContructor in login response dto

--- backend/src/main/java/edu/cit/auditor/paluto/authentication/LoginRequestDTO.java
+++ backend/src/main/java/edu/cit/auditor/paluto/authentication/LoginRequestDTO.java
@@ -1,8 +1,12 @@
 package edu.cit.auditor.paluto.authentication;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
     private String email;
     private String password;
