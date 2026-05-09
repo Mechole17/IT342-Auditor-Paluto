a4d98f2c267bcba5e5a314212da20a28dde22cc8
Mechole17 <232763660+Mechole17@users.noreply.github.com>
2026-05-09 12:59:47 +0800

    refactor: Establish web core foundation of layout, context, and components in core folder

---
+++ web/paluto/src/core/context/protectedRoutes.jsx
@@ -0,0 +1,14 @@
+// src/components/ProtectedRoute.jsx
+import { Navigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+export default function ProtectedRoute({ children, allowedRoles }) {
+    const { user, loading } = useAuth();
+
+    if (loading) return null;
+
+    if (!user) return <Navigate to="/" replace />;//replace later to a error page or login page
+    if (!allowedRoles.includes(user.role)) return <Navigate to="/" replace />;
+
+    return children;
+}
